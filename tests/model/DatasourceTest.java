257f04ba2f10f19188841cdb17b0e445d3f11dd7
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2024-01-09 19:23:52 +0100

    More unit tests

--- tests/model/DatasourceTest.java
+++ tests/model/DatasourceTest.java
@@ -5,15 +5,44 @@ import junit.framework.TestCase;
 public class DatasourceTest extends TestCase {
 
     Datasource datasource = new Datasource();
-    public void testAddingTransaction() {
+
+    // Przecinek w statystykach
+    public void testAddingTransaction1() {
         datasource.open();
-        boolean result = datasource.addingTransaction("Tyler", "Johnson", "PG", 1, "43.1", "1", "1");
+        boolean result = datasource.addingTransaction("Tyler", "Johnson", "PG", 1, "43,1", "1", "1");
         assertTrue(result);
         }
+    // Jedna niepoprawna(ujemna) wartość w statystykach
+    public void testAddingTransaction2() {
+        datasource.open();
+        boolean result = datasource.addingTransaction("Tyler", "Herro", "SF", 6, "43,1", "-1", "1");
+        assertFalse(result);
+    }
 
-    public void testUpdateStats() {
+    // Jedna niepoprawna (litera) wartość w statystykach
+    public void testAddingTransaction3() {
+        datasource.open();
+        boolean result = datasource.addingTransaction("Tyler", "Knocks", "SG", 7, "43,1a", "1", "1");
+        assertFalse(result);
+    }
+
+    // Aktualizowanie statystyk dla ujemnych wartości
+    public void testUpdateStats1() {
         datasource.open();
         int result = datasource.updateStats("-3", "1", "3", 1);
+        assertEquals(-1, result);
+    }
+    // Aktualizowanie dla statystyk z przecinkiem
+    public void testUpdateStats2() {
+        datasource.open();
+        int result = datasource.updateStats("3,7", "1", "3", 1);
+        assertEquals(1, result);
+    }
+
+    // Aktualizowanie dla statystyk z literą
+    public void testUpdateStats3() {
+        datasource.open();
+        int result = datasource.updateStats("3.7", "1a", "3", 1);
         assertEquals(-1, result);
     }
 }
